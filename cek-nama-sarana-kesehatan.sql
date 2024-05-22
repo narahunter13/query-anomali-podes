4d8c91dc6f72adf365db9ae8bdf0c5c00cc014fc
narahunter13 <narahunter13@gmail.com>
2024-05-22 14:58:43 +0700

    Update query (error hanya menampilkan 1 sarana)

--- cek-nama-sarana-kesehatan.sql
+++ cek-nama-sarana-kesehatan.sql
@@ -1,4 +1,4 @@
 SELECT t1.level_2_name AS KAB_KOTA, t1.level_3_name AS KECAMATAN, t1.level_4_name AS DESA, LEFT(t1.pair_label, LEN(t1.pair_label) - 2) AS NAMA
 FROM tnk_e7a35bd4.pair_label_value_1 t1
-WHERE t1.data_key = 'r704k2_prelist' AND pair_value = '1'
+WHERE t1.data_key = 'r704k2_prelist' AND pair_value <> '0'
 ORDER BY t1.level_2_name, t1.level_3_name, t1.level_4_name, t1.index1, t1.pair_label
