03c2b853c8ce7b2695091b0d39e9b30352d5741b
narahunter13 <narahunter13@gmail.com>
2024-05-28 09:59:45 +0700

    Fix error

--- cek-nama-sarana-perbankan.sql
+++ cek-nama-sarana-perbankan.sql
@@ -1,4 +1,4 @@
 SELECT t1.level_2_name AS KAB_KOTA, t1.level_3_name AS KECAMATAN, t1.level_4_name AS DESA, LEFT(t1.pair_label, LEN(t1.pair_label) - 2) AS NAMA
 FROM tnk_e7a35bd4.pair_label_value_0 t1
 WHERE t1.data_key IN ('r1205a1_prelist', 'r1205a2_prelist', 'r1205a3_prelist') AND pair_value <> '0'
-ORDER BY t1.level_2_name, t1.level_3_name, t1.level_4_name, t1.index1, t1.pair_label
+ORDER BY t1.level_2_name, t1.level_3_name, t1.level_4_name, t1.pair_label
