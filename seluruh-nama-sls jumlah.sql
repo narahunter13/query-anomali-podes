15c4d2830e41315e310449a1f34a3748afdf2ad8
narahunter13 <narahunter13@gmail.com>
2024-05-30 15:09:05 +0700

    Update format

--- seluruh-nama-sls jumlah.sql
+++ seluruh-nama-sls jumlah.sql
@@ -1,5 +1,5 @@
 SELECT t1.level_1_name AS Provinsi, t1.level_2_name AS Kab_Kota, t1.level_3_name AS Kecamatan, t1.level_4_name AS Desa, 
-STRING_AGG(SUBSTRING(t1.pair_label, CHARINDEX('R', t1.pair_label), 5), ',') WITHIN GROUP (ORDER BY CAST(t1.pair_value AS INTEGER) ASC) AS SLS_2024, t2.r106c AS Jumlah_SLS_2024
+STRING_AGG(SUBSTRING(t1.pair_label, CHARINDEX('R', t1.pair_label), CHARINDEX('#', t1.pair_label) - CHARINDEX('R', t1.pair_label)), ',') WITHIN GROUP (ORDER BY CAST(t1.pair_value AS INTEGER) ASC) AS SLS_2024, t2.r106c AS Jumlah_SLS_2024
 FROM tiv_6a73e7cb.pair_label_value_0 t1
 LEFT JOIN tiv_6a73e7cb.root_table t2
     ON t1.assignment_id = t2.assignment_id
--- seluruh-nama-sls.sql
+++ seluruh-nama-sls.sql
@@ -1,4 +1,4 @@
-SELECT t1.level_1_name AS Provinsi, t1.level_2_name AS Kab_Kota, t1.level_3_name AS Kecamatan, t1.level_4_name AS Desa, SUBSTRING(t1.pair_label, CHARINDEX('R', t1.pair_label), 5) AS SLS
+SELECT t1.level_1_name AS Provinsi, t1.level_2_name AS Kab_Kota, t1.level_3_name AS Kecamatan, t1.level_4_name AS Desa, SUBSTRING(t1.pair_label, CHARINDEX('R', t1.pair_label), CHARINDEX('#', t1.pair_label) - CHARINDEX('R', t1.pair_label)) AS SLS
 FROM tiv_6a73e7cb.pair_label_value_0 t1
 WHERE t1.data_key = 'r201_prelist' AND t1.pair_value <> '0'
 GROUP BY t1.level_1_name, t1.level_2_name, t1.level_3_name, t1.level_4_name, CAST(t1.pair_value AS INTEGER), t1.pair_label
